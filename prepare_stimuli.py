606aeb60093bd64e135787e54a6d4e58baa3a98b
fredcallaway <fredc@llaway.com>
2021-01-06 19:22:34 -0800

    center crop images

--- prepare_stimuli.py
+++ prepare_stimuli.py
@@ -5,6 +5,7 @@ import json
 def resize(image_pil, width, height):
     '''
     Resize PIL image keeping ratio and using white background.
+
     https://stackoverflow.com/questions/44370469/python-image-resizing-keep-proportion-add-white-background
     '''
     ratio_w = width / image_pil.width
@@ -23,6 +24,59 @@ def resize(image_pil, width, height):
     background.paste(image_resize, offset)
     return background.convert('RGB')
 
+def resize_and_crop(img, size, crop_type='middle'):
+    """
+    Resize and crop an image to fit the specified size.
+    args:
+        img_path: path for the image to resize.
+        size: `(width, height)` tuple.
+        crop_type: can be 'top', 'middle' or 'bottom', depending on this
+            value, the image will cropped getting the 'top/left', 'midle' or
+            'bottom/rigth' of the image to fit the size.
+    raises:
+        Exception: if can not open the file in img_path of there is problems
+            to save the image.
+        ValueError: if an invalid `crop_type` is provided.
+
+    adapted from https://gist.github.com/sigilioso/2957026
+    """
+
+    # If height is higher we resize vertically, if not we resize horizontally
+    # Get current and desired ratio for the images
+    img_ratio = img.size[0] / img.size[1]
+    ratio = size[0] / float(size[1])
+    #The image is scaled/cropped vertically or horizontally depending on the ratio
+    if ratio > img_ratio:
+        img = img.resize((size[0], size[0] * img.size[1] // img.size[0]),
+                Image.ANTIALIAS)
+        # Crop in the top, middle or bottom
+        if crop_type == 'top':
+            box = (0, 0, img.size[0], size[1])
+        elif crop_type == 'middle':
+            box = (0, (img.size[1] - size[1]) // 2, img.size[0], (img.size[1] + size[1]) // 2)
+        elif crop_type == 'bottom':
+            box = (0, img.size[1] - size[1], img.size[0], img.size[1])
+        else :
+            raise ValueError('ERROR: invalid value for crop_type')
+        img = img.crop(box)
+    elif ratio < img_ratio:
+        img = img.resize((size[1] * img.size[0] // img.size[1], size[1]),
+                Image.ANTIALIAS)
+        # Crop in the top, middle or bottom
+        if crop_type == 'top':
+            box = (0, 0, size[0], img.size[1])
+        elif crop_type == 'middle':
+            box = ((img.size[0] - size[0]) // 2, 0, (img.size[0] + size[0]) // 2, img.size[1])
+        elif crop_type == 'bottom':
+            box = (img.size[0] - size[0], 0, img.size[0], img.size[1])
+        else :
+            raise ValueError('ERROR: invalid value for crop_type')
+        img = img.crop(box)
+    else:
+        img = img.resize((size[0], size[1]),
+                Image.ANTIALIAS)
+    return img
+
 # %% --------
 os.system('rm -rf static/stimuli')
 
@@ -32,7 +86,7 @@ for cat in os.listdir('image_stimuli'):
     image_paths[cat] = []
     for i, fn in enumerate(os.listdir(f'image_stimuli/{cat}')):
         img = Image.open(f'image_stimuli/{cat}/{fn}')
-        new = resize(img, 300, 300)
+        new = resize_and_crop(img, (300, 300))
         path = f'static/stimuli/images/{cat}/{fn}'
         image_paths[cat].append(path)
         new.save(path)
