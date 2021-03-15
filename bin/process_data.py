7682ca4ab6ceae7bb6166d7b7fb7f482fd87a673
fredcallaway <fredc@llaway.com>
2021-03-14 18:33:54 -0700

    parsing 2AFC data

--- bin/process_data.py
+++ bin/process_data.py
@@ -9,6 +9,8 @@ import json
 from nltk.stem.wordnet import WordNetLemmatizer
 lemmatize = WordNetLemmatizer().lemmatize
 
+import bonus
+
 def load_words():
     stimuli = json.load(open('static/stimuli/stimuli.json'))
     return {kind: set(map(lemmatize, words)) 
@@ -41,15 +43,14 @@ for w in ALL_WORDS:
     assert spell.candidates(w) == {w}, w
     assert classify_response(w, w), w
 
-
-def parse_trial(row):
+def parse_simple(row):
     ev = literal_eval(row.events)
     t = literal_eval(row.trial)
     x = {'wid': row.wid, 'word': t['word'], 'image': t['image'], 'practice': t.get('practice', False)}
     x['word_type'] = classify_word(x['word'])
 
     # import IPython, time; IPython.embed(); time.sleep(0.5)
-    import IPython, time; IPython.embed(); time.sleep(0.5)
+    # import IPython, time; IPython.embed(); time.sleep(0.5)
 
     for e in ev:
         # if e['event'] == 'start trial':
@@ -70,6 +71,26 @@ def parse_trial(row):
             return x
     assert False
 
+
+def parse_afc(row):
+    ev = literal_eval(row.events)
+    t = literal_eval(row.trial)
+    if abs(row.rt - (ev[1]['time'] - ev[0]['time'])) > 30:
+        print('RT discrepancy', row.rt - (ev[1]['time'] - ev[0]['time']))
+    assert row.correct == (ev[-1]['response'] == t['target_image'])
+
+    return {
+        'wid': row.wid,
+        'practice': t.get('practice', False),
+        'rt': row.rt,
+        'word': t['word'],
+        'target': t['target_image'][25:-4],
+        'lure': t['lure_images'][0][25:-4],
+        'key': ev[-1]['key'],
+        'correct': row.correct
+    }
+
+
 def main(codeversion):
     out = f'data/processed/{codeversion}/'
     os.makedirs(out, exist_ok=True)
@@ -77,8 +98,11 @@ def main(codeversion):
     def load_raw(kind):
         return  pd.read_csv(f'data/human/{codeversion}/{kind}.csv').dropna(axis=1)
 
-    trials = load_raw('simple-recall').apply(parse_trial, axis=1, result_type='expand')
-    trials.set_index('wid').to_csv(out + 'trials.csv')
+    simple = load_raw('simple-recall').apply(parse_simple, axis=1, result_type='expand')
+    simple.set_index('wid').to_csv(out + 'simple-recall.csv')
+    
+    afc = load_raw('afc').apply(parse_afc, axis=1, result_type='expand')
+    afc.set_index('wid').to_csv(out + 'afc.csv')
 
     pdf = load_raw('participants').set_index('wid')
     pdf['math_correct'] = load_raw('math').set_index('wid').num_correct
@@ -87,6 +111,14 @@ def main(codeversion):
     survey_raw = load_raw('survey-text').set_index('wid').responses
     survey = pd.DataFrame(list(survey_raw.apply(literal_eval)))
     survey.to_csv(out + 'survey.csv')
+
+    if os.path.isdir('/Users/fred/Projects/memory/data/'):    
+        os.system(f'rsync -av data/processed/{codeversion}/ /Users/fred/Projects/memory/data/{codeversion}/')
+
+    bonus.main(codeversion)
+
+
+
 
 
 if __name__ == '__main__':
